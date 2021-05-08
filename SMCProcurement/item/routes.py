6e3914e6b8921ebb4064e7c5553be0174bb1cc0c
Marjone <marjonedev@gmail.com>
2021-05-08 12:03:51 +0800

    inventory create new item

--- SMCProcurement/item/routes.py
+++ SMCProcurement/item/routes.py
@@ -10,11 +10,13 @@ from SMCProcurement.item.forms import ItemForm
 from SMCProcurement.models import ItemCategory, Supplier
 from SMCProcurement.models.item import Item
 
+
 @blueprint.route('/items')
 @login_required
 def items():
     items = Item.query.all()
     return render_template('items/index.html', items=items)
+
 
 @blueprint.route('/items/create', methods=["GET", "POST"])
 @login_required
@@ -33,6 +35,7 @@ def create_item():
     else:
         return render_template('items/create.html', form=form)
 
+
 @blueprint.route('/items/<id>/edit', methods=["GET", "POST"])
 @login_required
 def edit_item(id):
@@ -50,10 +53,10 @@ def edit_item(id):
     else:
         return render_template('items/edit.html', form=form, obj=item)
 
+
 @blueprint.route('/items/<id>/delete', methods=["POST"])
 @login_required
 def delete_item(id):
-
     if 'delete_item' in request.form.to_dict():
         try:
             item = db.session.query(Item).get(id)
@@ -66,6 +69,7 @@ def delete_item(id):
             flash('Error: Unable to delete item, {}. '.format(msg), "error")
             return redirect(url_for('item_blueprint.items'))
 
+
 @blueprint.route('/items/<id>', methods=["GET"])
 @login_required
 def view_item(id):
@@ -75,10 +79,10 @@ def view_item(id):
     except:
         return render_template('page-404.html'), 404
 
+
 @blueprint.route('/api/items', methods=["POST"])
 @login_required
 def get_items_json():
-
     # query = request.form["q"]
     # filter = request.form["filter"]
     # page = request.form["p"] if request.form["p"] else 0
@@ -119,3 +123,16 @@ def get_items_json():
     response = dict(data=[i.toDataTable() for i in items])
 
     return jsonify(response)
+
+
+@blueprint.route('/api/items/create', methods=["POST"])
+@login_required
+def api_create_item():
+    if 'create_item' in request.form:
+        item = Item(**request.form)
+        db.session.add(item)
+        db.session.commit()
+
+        return jsonify(dict(status="success", data=item.toDataTable(), message="Success! item has been created."))
+    else:
+        return jsonify(dict(status="error", data={}, message="Error. This event is restricted.")), 400
---
+++ SMCProcurement/static/assets/js/inventory.js
@@ -0,0 +1,125 @@
+$(function(){
+    function appendItemToInventory(id, data){
+        let item_code = data.item_code == "None" ? "" : data.item_code;
+        let brand = data.brand == "None" ? "" : data.brand;
+        let model = data.model == "None" ? "" : data.model;
+        let str = '<tr id="lines_'+id+'">' +
+            '<td>'+data.name+'</td>' +
+            '<td>'+item_code+'</td>' +
+            '<td>'+brand+'</td>' +
+            '<td>'+model+'</td>' +
+            '<td><input class="form-control form-control-sm flatpickr" id="inventory_items-'+id+'-purchased_date" name="inventory_items-'+id+'-purchased_date" type="text" value=""></td>' +
+            '<td class="text-right">&#8369; '+data.unit_price+'</td>' +
+            '<td><input class="form-control form-control-sm" id="inventory_items-'+id+'-unit_price" name="inventory_items-'+id+'-qty" type="number" value="" required></td>' +
+            '<td>' +
+            '<input id="inventory_items-'+id+'-id" name="inventory_items-'+id+'-item_id" type="hidden" value="'+id+'">' +
+            '<button type="button" role="button" class="btn delete btn-danger feather icon-delete"></button>' +
+            '</td>' +
+            '</tr>';
+
+        $("#inventoryItems").find("tbody").append(str);
+        $('#inventory_items-'+id+'-purchased_date').flatpickr({
+            minDate: "today",
+            altInput: true,
+            defaultDate: "today",
+            altFormat: "F j, Y",
+            dateFormat: "Y-m-d"
+        });
+        $('#addItemModal').modal("hide");
+    }
+
+    $('button#addItem').on('click', function (e){
+      e.preventDefault();
+      $('#addItemModal').modal("show")
+    });
+
+    $('#addItemModal').on('show.bs.modal', function (e) {
+        $('#searchItemTable').DataTable({
+            "ajax": {
+                "url": "/api/items",
+                "dataType": "json",
+                "method": "POST",
+                "dataSrc": "data",
+                "contentType":"application/json"
+            },
+            "columns": [
+                {"data": "name"},
+                {"data": "item_code", render: function (data){
+                    return data == "None" ? "" : data;
+                }},
+                {"data": "brand", "visible": false, "searchable": true},
+                {"data": "model", "visible": false, "searchable": true},
+                {"data": "serial", "visible": false, "searchable": true},
+                {"data": "description", "visible": false, "searchable": true},
+                {"data": "category"},
+                {"data": "supplier"},
+                {"data": "qty", className: "text-right", render: function (data){
+                    return data == "None" ? 0 : data;
+                }},
+                {"data": "unit_price", className: "text-right", "render": function (data, type, row, meta){
+                        return  "&#8369; "+ data;
+                }},
+                {"data": "id", "render": function (data, type, row, meta){
+                    str = "<input class='item-data' type='hidden' value='"+JSON.stringify(row)+"'>";
+                    str += "<button class='btn btn-dark btn-sm select-item' data-id='"+data+"'>Select</button>";
+                    return str
+                }}
+            ]
+        });
+    });
+
+    $('#addItemModal').on('hidden.bs.modal', function (e) {
+      $('#searchItemTable').DataTable().clear().destroy();
+      $('#createNewItemForm').trigger("reset");
+      $('#existingItemTab').tab('show');
+      $('.new-error').text("").hide();
+    });
+
+    $('a.new-item-tab[data-toggle="pill"]').on('shown.bs.tab', function (e) {
+        $('input[name=is_existing]').val($(e.target).data('existing'))
+    });
+
+    $(document).on("click", ".select-item", function (e){
+        e.preventDefault();
+        let id = $(this).data("id");
+        let data = $(this).closest("tr").find(".item-data").val();
+        data = JSON.parse(data);
+        appendItemToInventory(id, data);
+    });
+
+    $("#searchExistingForm").on("submit", function(e){ e.preventDefault(); return false; });
+
+    $(document).on('click', 'button.delete', function (e){
+        e.preventDefault();
+        $(this).closest("tr").remove();
+    });
+
+    $("#createNewItemForm").on('submit', function (e){
+        e.preventDefault();
+        let $this = $(this);
+        let data = {};
+        $.each($this.serializeArray(), function(i, field) {
+            data[field.name] = field.value;
+        });
+        data["create_item"] = 1;
+
+        $.ajax({
+            url: '/api/items/create',
+            data: data,
+            type: "POST",
+            success: function(response){
+                if(response.status == "success"){
+                    appendItemToInventory(response.data.id, response.data);
+                }
+            },
+            error: function (response){
+                let data = response.responseJSON;
+                if(data.status == "error"){
+                    $('.new-error').text(data.message).show();
+                }
+            }
+        });
+
+        return false;
+    });
+});
